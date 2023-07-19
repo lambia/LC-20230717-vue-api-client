e6ef98731bc166718d24842a6690abfd9fdd8d46
Luca Lambiase <luca.lambia@gmail.com>
2023-07-19 12:09:11 +0200

    refactor(header): create a separate component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   name: "Vue API Client",
-  data() {
-    return {
-      navLinks: [
-        { label: "Home", name: "home" },
-        { label: "About us", name: "about"  },
-        { label: "Posts", name: "posts" },
-        // { label: "Testa pagina di errore", name: "error", params: { code: 404 } },
-        // { label: "Articolo a caso", name: "single-post", params: { id: 3 } }
-      ]
-    }
+  components: {
+    AppHeader
   }
 }
 </script>
 
 <template>
   
-  <header>
-    <template v-for="i in navLinks">
-      <router-link :to="{ name: i.name, params: i.params }">{{ i.label }}</router-link>
-      <!-- <router-link :to="i">{{ i.label }}</router-link> -->
-    </template>
-  </header>
+  <AppHeader />
 
   <router-view></router-view>
 
 </template>
 
-<style scoped>
-header {
-  width: 100%;
-  background: #666;
-  color: black;
-  padding: 1rem;
-}
-header a {
-  margin: 1rem;
-}
-</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+        navLinks: [
+            { label: "Home", name: "home" },
+            { label: "About us", name: "about"  },
+            { label: "Posts", name: "posts" },
+            // { label: "Testa pagina di errore", name: "error", params: { code: 404 } },
+            // { label: "Articolo a caso", name: "single-post", params: { id: 3 } }
+        ]
+        }
+    }
+}
+</script>
+
+<template>
+  <header>
+    <template v-for="i in navLinks">
+      <router-link :to="{ name: i.name, params: i.params }">{{ i.label }}</router-link>
+      <!-- <router-link :to="i">{{ i.label }}</router-link> -->
+    </template>
+  </header>
+</template>
+
+
+<style scoped>
+header {
+  width: 100%;
+  background: #666;
+  color: black;
+  padding: 1rem;
+}
+header a {
+  margin: 1rem;
+}
+</style>
