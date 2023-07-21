110d2889669ca5f924ae1206636632d988a5a52b
Luca Lambiase <luca.lambia@gmail.com>
2023-07-21 16:36:04 +0200

    feat: add mail form

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,6 +1,11 @@
 <script>
+import ContactForm from "../components/ContactForm.vue";
+
 export default {
     name: "AppHome",
+    components: {
+        ContactForm
+    },
     data() {
         return {
         }
@@ -13,6 +18,7 @@ export default {
     <section>
         <h1>Welcome</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem velit et asperiores ab illum, labore minus deleniti. Voluptatum sunt veniam culpa recusandae consequatur, reiciendis, nam, quaerat iusto velit porro fuga.</p>
+        <ContactForm />
     </section>
 
 </template>
